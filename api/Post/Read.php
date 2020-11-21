59b43a5d5e02c9cb3a8bd9d045c63019e8e5c3e0
Nwankwo Samuel <nwankwosami@gmail.com>
2020-11-21 02:45:14 +0100

    finished read

--- api/Post/Read.php
+++ api/Post/Read.php
@@ -3,5 +3,47 @@
 header('Access-Control-Allow-Origin: *');
 header('Content-Type: application/json');
 
+include_once '../../config/Database.php';
+include_once '../../models/post.php';
+
+  // Instantiate DB & connect
+$database = new Database();
+$db = $database->connect();
+
+  // Instantiate blog post object
+$post = new Post($db);
+
+// blog post query
+$result = $post->read();
+
+// get row count
+$num = $result->rowCount();
+
+if($num > 0){
+
+    $posts_arr = array();
+    $posts_arr['data'] = array();
+
+    while($row = $result->fetch(PDO::FETCH_ASSOC)){
+        extract($row);
+
+        $post_item = array(
+            'id' => $id,
+            'title' => $title,
+            'body' => html_entity_decode($body),
+            'author' => $author;
+            'category_id' => $category_id,
+            'category_name' => $category_name;
+        );
+
+        // push to data
+        array_push($posts_arr['data'], $post_item);
+    }
+    // turn it to json
+    echo json_encode($posts_arr);
+} else {
+    // No Post
+    echo json_encode(array('message' -> 'No Post found'));
+}
 
 ?>
